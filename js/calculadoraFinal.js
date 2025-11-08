ec36ca0bc23adb024754c7d1416d2b6c440c22c2
Rensso26 <rnparra@uce.edu.ec>
2025-11-07 19:42:02 -0500

    Taller 8

---
+++ js/calculadoraFinal.js
@@ -0,0 +1,32 @@
+let num1=0;
+let num2=0;
+
+function mostrarDisplay(valor){
+    let elemento = document.getElementById('display');
+    elemento.innerText = elemento.innerText + valor;
+}
+
+function limpiarDisplay(){
+    let elemento = document.getElementById('dispay');
+    elemento.innerText = '';
+}
+
+function sumar(){
+
+}
+
+function restart(){
+
+}
+
+function multiplicar() {
+
+}
+
+function dividir() {
+
+}
+
+function porcentaje() {
+
+}
